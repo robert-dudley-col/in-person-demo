b6b2f83cc6f8cf45392f05287eca0233cbe5a887
robert-dudley-col <robert.thompson@dudleycol.ac.uk>
2025-02-14 10:07:40 +0000

    Display booking

--- backend/routes/bookings/index.js
+++ backend/routes/bookings/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const {MongoClient, Collection} = require('mongodb');
 var url = 'mongodb://localhost:27017/';
 var AuthFunctions = require('../auth/functions');
+var mongo = require('mongodb');
 
 router.post('/',async function(req,res){
     try{
@@ -50,6 +51,47 @@ router.post('/',async function(req,res){
     }catch(error){
         console.log(error);
         res.status(500).json({message:"Could not create booking"})
+    }
+})
+
+
+router.get('/:id', async function(req,res){
+    try {
+        var token = req.headers['authorization'];
+        if(AuthFunctions.authenticateToken(token))
+        {
+            var user = await AuthFunctions.getIDFromAuth(token);
+
+            const client = new MongoClient(url);
+            const database = client.db('hotel');
+            
+            var booking = await database.collection('bookings').findOne({
+                _id:new mongo.ObjectId(req.params.id)
+            });
+
+            if(user == booking.user.toString())
+            {
+                var hotel = await database.collection('hotels').findOne({
+                    _id:new mongo.ObjectId(booking.hotel)
+                })
+
+                var user = await database.collection('users').findOne({
+                    _id:booking.user
+                })
+                delete user.password
+                
+                booking.user = user;
+                booking.hotel = hotel;
+                res.json(booking);
+            }else{
+                res.status(403).json({message:"This is not the booking you're looking for 👀"});
+            }
+        }else{
+            res.status(403).json({message:"You are not logged in"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Could not load booking"});
     }
 })
 
--- frontend/src/routes/bookings/id.jsx
+++ frontend/src/routes/bookings/id.jsx
@@ -1,25 +1,56 @@
 import { useParams } from "react-router-dom";
 import MainNavigation from "../../components/MainNavigation";
 import { Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 
 export default function ViewBooking()
 {
     let {bookingID} = useParams();
+    const [booking, setBooking] = useState({});
+    const [cookies] = useCookies(['token']);
 
-    return(
-        <>
-            <MainNavigation/>
-            <Container>
-                <Row>
-                    <Col>
-                        <p>{bookingID}</p>
+    useEffect(()=>{
+        axios.get('http://localhost:3000/bookings/'+bookingID,{
+            headers:{
+                Authorization:'Bearer '+cookies['token']
+            }
+        }).then((res)=>{
+            setBooking(res.data)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    },[]);
 
-                        
-                    
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
+    if(booking._id !== undefined)
+    {
+        return(
+            <>
+                <MainNavigation/>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>{booking.hotel.location}</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    }else{
+        return(
+            <>
+                <MainNavigation/>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>Loading Booking</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    }
+    
 }
